34ba13cb05c9a8f1e1e7b5807e94609f705b9d6b
Eino Hirvi <eino.hirvi@gmail.com>
2026-04-06 14:59:30 +0300

    client: add Heading and improve Button component

--- packages/client/src/views/Home/index.tsx
+++ packages/client/src/views/Home/index.tsx
@@ -1,12 +1,13 @@
 import { Stack } from "../../components/Stack";
 import { Button } from "../../components/Button";
+import { Heading } from "../../components/Heading";
 
 export const Home = () => {
   const onCreate = () => {};
 
   return (
     <Stack vertical gap={2}>
-      <p>Canvas Paint</p>
+      <Heading>Canvas Paint</Heading>
       <Stack vertical gap={2}>
         <Button onClick={onCreate}>Create a new paint session</Button>
         <Button>Join a paint session</Button>
