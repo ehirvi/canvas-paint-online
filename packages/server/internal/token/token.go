7fb0b2d176a3f909acf37f08f3382b57ffd02815
Eino Hirvi <eino.hirvi@gmail.com>
2026-04-12 20:19:23 +0300

    server: verify access token

--- packages/server/internal/token/token.go
+++ packages/server/internal/token/token.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"fmt"
 	"online-canvas-paint-server/internal/common"
 	"online-canvas-paint-server/internal/user"
 	"os"
@@ -8,14 +9,45 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
+type CustomJwtClaims struct {
+	SessionID string `json:"sessionId"`
+	UserID    string `json:"userId"`
+	UserRole  string `json:"userRole"`
+	jwt.RegisteredClaims
+}
+
 func CreateAccessToken(user *user.User, sessionId common.ID) string {
 	secret := []byte(os.Getenv("SIGNING_SECRET"))
 
-	jwt := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"sessionId": sessionId.String(),
-		"userId":    user.ID.String(),
-		"userRole":  string(user.Role),
+	jwt := jwt.NewWithClaims(jwt.SigningMethodHS256, CustomJwtClaims{
+		SessionID: sessionId.String(),
+		UserID:    user.ID.String(),
+		UserRole:  string(user.Role),
 	})
 	token, _ := jwt.SignedString(secret)
 	return token
+}
+
+func VerifyAccessToken(tokenPayload []byte) *CustomJwtClaims {
+	secret := []byte(os.Getenv("SIGNING_SECRET"))
+	tokenString := string(tokenPayload)
+	token, err := jwt.ParseWithClaims(tokenString, &CustomJwtClaims{}, func(token *jwt.Token) (any, error) {
+		return secret, nil
+	}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
+
+	if err != nil {
+		fmt.Printf("jwt error: %s\n", err)
+	}
+
+	if !token.Valid {
+		fmt.Println("jwt invalid or expired")
+	}
+
+	claims, ok := token.Claims.(*CustomJwtClaims)
+
+	if !ok {
+		fmt.Println("could not get jwt claims")
+	}
+
+	return claims
 }
