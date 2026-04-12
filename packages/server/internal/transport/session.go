cd573aa89bd3ba16cde0e4da2426f09311d94ecc
Eino Hirvi <eino.hirvi@gmail.com>
2026-04-12 20:22:36 +0300

    server: read messages from webtransport stream

--- packages/server/internal/transport/session.go
+++ packages/server/internal/transport/session.go
@@ -6,8 +6,18 @@ import (
 
 	"github.com/quic-go/webtransport-go"
 )
+
+func readStreamMessage(stream *webtransport.Stream) (*message.Message, error) {
+	msg, _ := message.DecodeMessage(stream)
+	return msg, nil
+}
+
 func handleStream(manager *session.Manager, session *webtransport.Session, stream *webtransport.Stream) {
 	defer stream.Close()
+
+	for {
+		msg, _ := readStreamMessage(stream)
+	}
 }
 
 func UpgradeToWebTransportSession(manager *session.Manager, wtServer *webtransport.Server, w http.ResponseWriter, r *http.Request) {
