69ba95e0848a1734b0b0064345d8119a2c312a2d
Eino Hirvi <eino.hirvi@gmail.com>
2026-04-18 15:49:47 +0300

    client: add empty canvas element to paint on

--- packages/client/src/components/Stack/index.tsx
+++ packages/client/src/components/Stack/index.tsx
@@ -1,20 +1,23 @@
 import type React from "react";
 import styled from "styled-components";
 
-const StyledStack = styled.div<{ $vertical?: boolean, $gap?: number }>`
+const StyledStack = styled.div<{ $vertical?: boolean; $gap?: number }>`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: ${(p) => p.$vertical ? "column" : "row"};
-  gap: ${p => p.$gap ? p.$gap : 0}rem;
-
+  flex-direction: ${(p) => (p.$vertical ? "column" : "row")};
+  gap: ${(p) => (p.$gap ? p.$gap : 0)}rem;
 `;
 
 type TStackProps = React.PropsWithChildren & {
-  vertical?: boolean
-  gap?: number
+  vertical?: boolean;
+  gap?: number;
 };
 
 export const Stack = ({ vertical, gap, children }: TStackProps) => {
-  return <StyledStack $vertical={vertical} $gap={gap}>{children}</StyledStack>;
+  return (
+    <StyledStack $vertical={vertical} $gap={gap}>
+      {children}
+    </StyledStack>
+  );
 };
--- packages/client/src/views/PaintCanvas/index.tsx
+++ packages/client/src/views/PaintCanvas/index.tsx
@@ -1,14 +1,61 @@
-import { useParams } from "react-router";
-import { getAccessToken } from "../../utils/storage";
+import styled, { keyframes } from "styled-components";
+import { Stack } from "../../components/Stack";
+import { useEffect, useRef } from "react";
+
+const CANVAS_WIDTH = 1280;
+const CANVAS_HEIGHT = 960;
+
+const fadeInAnimation = keyframes`
+  from {
+      transform: translateY(-25%);
+      opacity: 0
+  }
+  to {
+      transform: translateY(0%);
+      opacity: 1
+  }
+`;
+
+const AnimatedRoot = styled.div`
+  animation: ${fadeInAnimation} ease-out 500ms 1;
+`;
+
+const StyledShadow = styled.div`
+  background-color: #915a33;
+  width: ${CANVAS_WIDTH}px;
+  height: ${CANVAS_HEIGHT}px;
+  border-radius: 0.5rem;
+  position: absolute;
+  transform: translateY(0.5rem);
+`;
+
+const StyledCanvas = styled.canvas`
+  position: relative;
+  border-radius: 0.5rem;
+`;
 
 export const PaintCanvas = () => {
-  const { sessionId } = useParams();
-  const accessToken = getAccessToken();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d")!;
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+  }, []);
+
   return (
-    <>
-      <p>paint canvas</p>
-      <p>sessionId: {sessionId}</p>
-      <p>accessToken: {accessToken}</p>
-    </>
+    <Stack>
+      <AnimatedRoot>
+        <StyledShadow />
+        <StyledCanvas
+          width={CANVAS_WIDTH}
+          height={CANVAS_HEIGHT}
+          ref={canvasRef}
+        />
+      </AnimatedRoot>
+    </Stack>
   );
 };
